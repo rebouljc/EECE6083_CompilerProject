77b62f5702935d9e74f0d9ec1cffc5913ecd2066
rebouljc <rebouljc@mail.uc.edu>
2021-02-28 16:43:12 -0500

    4:42 p.m. --Update.  <Relation> and <Term> non-terminals are now defined.  Only <factor>, <name>, and <argument_list> remain.

--- CompilerAppJamesCReboulet/Relation_.cpp
+++ CompilerAppJamesCReboulet/Relation_.cpp
@@ -1,6 +1,7 @@
 #include "Relation_.h"
 #include "TerminalNode.h"
 #include "Identifier.h"
+#include "Term.h"
 
 //2-23-2021: Code needs to be modified.  This is type mark code.
 Relation_::Relation_(Parser* parser, ParseTreeNode* motherNode)
@@ -11,7 +12,55 @@ Relation_::Relation_(Parser* parser, ParseTreeNode* motherNode)
 
 void Relation_::verifySyntaxCreateParseTree(int tokenCounter, ParseTreeNode* motherNode)
 {
+	//Note:  This same code will now work with relation, term, and factor.  It just needs to be modified for each of those non-terminal classes.
+	Token* currentToken = this->parserPtr->getCurrentlyReadToken();
+	if (currentToken->getTokenValue() == "<"  ||
+		currentToken->getTokenValue() == ">=" ||
+		currentToken->getTokenValue() == "<=" ||
+		currentToken->getTokenValue() == "==" ||
+		currentToken->getTokenValue() == "!=" )
+	{
+		this->linkedMemberNonterminals.push_back(new TerminalNode(currentToken));
+	}
 
+	//If this->linkedMemberNonterminals happens to be empty, we don't want to do an access and pass a null pointer to the dynamic_cast method
+	//That will probably cause a memory access violation to occur at runtime, because we are accessing a non-existent element in our vector.
+	else if (!this->linkedMemberNonterminals.empty() &&
+		dynamic_cast<Term*>(this->linkedMemberNonterminals.at(this->linkedMemberNonterminals.size() - 1)) != nullptr)
+	{
+		this->linkedMemberNonterminals.push_back(new Relation_(this->parserPtr, motherNode));
+		bool isValid = this->linkedMemberNonterminals.at(this->linkedMemberNonterminals.size() - 1);
+		if (!isValid)
+		{
+			this->linkedMemberNonterminals.pop_back();
+		}
+		//If we ever get here, we can set the isValid flag to true, since we know we have had an ArithOp added at least.
+		//No reason to check the size of the linkedMemberNonterminals each time and waste more CPU clock cycles and memory accesses.
+		this->setIsValid(true);
+		//UH! OH!  We had better return here, or we are going to get infinite recursion.
+		return;
+	}
+
+
+	else
+	{
+		this->linkedMemberNonterminals.push_back(new Term(this->parserPtr, motherNode));
+		bool isValid = this->linkedMemberNonterminals.at(this->linkedMemberNonterminals.size() - 1);
+		if (!isValid)
+		{
+			this->linkedMemberNonterminals.pop_back();
+			//Here, we return early, since we assume that ArithOp_ is empty if we don't create a valid <ArithOp>.
+			//We have to stop the infinite recursion from occurring.
+			return;
+
+		}
+
+	}
+
+	currentToken = this->parserPtr->readNextToken();
+	++tokenCounter;
+	this->verifySyntaxCreateParseTree(tokenCounter, motherNode);
+	return;
 }
 
 ParseTreeNode* Relation_::getNodePtr()
--- CompilerAppJamesCReboulet/Term.cpp
+++ CompilerAppJamesCReboulet/Term.cpp
@@ -1,6 +1,6 @@
 #include "Term.h"
-#include "TerminalNode.h"
-#include "Identifier.h"
+#include "Term_.h"
+#include "Factor.h"
 
 //2-23-2021: Code needs to be modified.  This is type mark code.
 Term::Term(Parser* parser, ParseTreeNode* motherNode)
@@ -11,7 +11,43 @@ Term::Term(Parser* parser, ParseTreeNode* motherNode)
 
 void Term::verifySyntaxCreateParseTree(int tokenCounter, ParseTreeNode* motherNode)
 {
+	//Note:  This same code will now work with relation, term, and factor.  It just needs to be modified for each of those non-terminal classes.
+	//If this->linkedMemberNonterminals happens to be empty, we don't want to do an access and pass a null pointer to the dynamic_cast method
+	//That will probably cause a memory access violation to occur at runtime, because we are accessing a non-existent element in our vector.
+	Token* currentToken = this->parserPtr->getCurrentlyReadToken();
 
+	if (!this->linkedMemberNonterminals.empty() &&
+		dynamic_cast<Factor*>(this->linkedMemberNonterminals.at(this->linkedMemberNonterminals.size() - 1)) != nullptr)
+	{
+		this->linkedMemberNonterminals.push_back(new Term_(this->parserPtr, motherNode));
+		//Expression_ can possibly be empty, so if it is, we just remove it from the parse tree to eliminate confusion.
+		bool isValid = this->linkedMemberNonterminals.at(this->linkedMemberNonterminals.size() - 1);
+		if (!isValid)
+		{
+			this->linkedMemberNonterminals.pop_back();
+		}
+
+		//Note:  If we ever get here, we know that we have seen a correctly added ArithOp, so we set isValid(true);
+		this->setIsValid(true);
+		return;
+	}
+
+	else
+	{
+		this->linkedMemberNonterminals.push_back(new Factor(this->parserPtr, motherNode));
+		bool isValid = this->linkedMemberNonterminals.at(this->linkedMemberNonterminals.size() - 1);
+		if (!isValid)
+		{
+			this->linkedMemberNonterminals.pop_back();
+			//Here, we return early, since there is no reason to continue if we don't have an arithOp.  It is a waste of resources.
+			return;
+		}
+
+	}
+
+	currentToken = this->parserPtr->readNextToken();
+	++tokenCounter;
+	this->verifySyntaxCreateParseTree(tokenCounter, motherNode);
 }
 
 ParseTreeNode* Term::getNodePtr()
